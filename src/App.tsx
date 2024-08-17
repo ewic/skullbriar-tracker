d6bc1fdaf338405360921224e23452a47d4421ca
Eric Zhang <ewicexclamationpoint@gmail.com>
2024-08-17 01:56:36 -0400

    power and toughness setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,40 @@
+import { useState } from 'react';
 import './App.css'
 
 function App() {
   
-  // const [ plusOne, setPlusOne ] = useState<Number>(0)
-  // const [ trample, setTrample ] = useState<Number>(0)
-  // const [ flying, setFlying ] = useState<Number>(0)
-  // const [ vigilance, setVigilance ] = useState<Number>(0)
-  // const [ deathtouch, setDeathtouch ] = useState<Number>(0)
-  // const [ menace, setMenace] = useState<Number>(0)
-  // const [ firststrike, setFirstStrike] = useState<Number>(0)
-  // const [ hexproof, setHexproof] = useState<Number>(0)
-  // const [ lifelink, setLifelink] = useState<Number>(0)
-  // const [ reach, setReach] = useState<Number>(0)
-  // const [ indestructible, setIndestructible] = useState<Number>(0)
+  const [ plusOne, setPlusOne ] = useState<number>(0)
+  // const [ trample, setTrample ] = useState<number>(0)
+  // const [ flying, setFlying ] = useState<number>(0)
+  // const [ vigilance, setVigilance ] = useState<number>(0)
+  // const [ deathtouch, setDeathtouch ] = useState<number>(0)
+  // const [ menace, setMenace] = useState<number>(0)
+  // const [ firststrike, setFirstStrike] = useState<number>(0)
+  // const [ hexproof, setHexproof] = useState<number>(0)
+  // const [ lifelink, setLifelink] = useState<number>(0)
+  // const [ reach, setReach] = useState<number>(0)
+  // const [ indestructible, setIndestructible] = useState<number>(0)
+
+  const [ power, setPower ] = useState<number>(1);
+  const [ toughness, setToughness ] = useState<number>(1);
+
+  function reset() {
+    setPlusOne(0);
+  }
 
   return (
     <main>
       <h1 className="font-extrabold">Skullbriar App</h1>
-      <div className="bg-[url('./assets/images/skullbriar-clear.png')] h-[680px] w-[480px] rounded-xl">
-        <p className="text-white h-full border">1</p>
+      <div className="relative bg-[url('./assets/images/skullbriar-clear.png')] h-[680px] w-[480px] rounded-xl">
+        <div className="absolute bottom-7 right-20">
+          <span className="text-black font-bold text-2xl">{power + plusOne}</span>
+        </div>
+        <div className="absolute bottom-7 right-10">
+          <span className="text-black font-bold text-2xl">{toughness + plusOne}</span>
+        </div>
       </div>
+      <button onClick={() => {setPlusOne(plusOne+1)}}>+1/+1</button>
+      <button onClick={() => reset()}>RESET</button>
     </main>
   )
 }
