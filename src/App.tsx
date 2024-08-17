dbeb95c9a7a7f29ded4c610156c3d533d23024bc
ewic <ewicexclamationpoint@gmail.com>
2024-08-16 22:32:07 -0400

    2nd commit

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import './App.css'
+import skullbriar from './assets/images/skullbriar.png'
+
+function App() {
+  
+  const [ plusOne, setPlusOne ] = useState<Number>(0)
+  const [ trample, setTrample ] = useState<Number>(0)
+  const [ flying, setFlying ] = useState<Number>(0)
+  const [ vigilance, setVigilance ] = useState<Number>(0)
+  const [ deathtouch, setDeathtouch ] = useState<Number>(0)
+  const [ menace, setMenace] = useState<Number>(0)
+  const [ firststrike, setFirstStrike] = useState<Number>(0)
+  const [ hexproof, setHexproof] = useState<Number>(0)
+  const [ lifelink, setLifelink] = useState<Number>(0)
+  const [ reach, setReach] = useState<Number>(0)
+  const [ indestructible, setIndestructible] = useState<Number>(0)
+
+  return (
+    <main>
+      <h1 className="font-extrabold">Skullbriar App</h1>
+      <div className="bg-[url('./assets/images/skullbriar-clear.png')] h-[680px] w-[480px] rounded-xl">
+        <p className="text-white h-full border">1</p>
+      </div>
+    </main>
+  )
+}
+
+export default App
